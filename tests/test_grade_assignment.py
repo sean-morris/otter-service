ba2ca3a004fdbc967a6870563213e7a9bc52d2a4
sean-morris <sean.smorris@gmail.com>
2025-01-05 13:02:26 -0800

    Updated tests

--- tests/test_grade_assignment.py
+++ tests/test_grade_assignment.py
@@ -3,6 +3,8 @@ import otter_service.grade_assignment as ga
 import os
 import shutil
 
+KEY_FILE_PATH = "test_files/gh_key.yaml"
+
 
 @pytest.fixture()
 def configure():
@@ -11,6 +13,10 @@ def configure():
     print("Removing Tree")
     if os.path.isdir("./materials-x22-private"):
         shutil.rmtree("./materials-x22-private")
+    if os.path.isdir("./Data88e-online-dev"):
+        shutil.rmtree("./Data88e-online-dev")
+    if os.path.isfile("./final_grades.csv"):
+        os.remove("./final_grades.csv")
 
 
 def test_download_autograder_materials(configure):
@@ -21,8 +27,9 @@ def test_download_autograder_materials(configure):
 
 
 @pytest.mark.asyncio
-async def test_grade_assignment(configure):
-    secrets_file = os.path.join(os.path.dirname(__file__), "test_files/gh_key.yaml")
+@pytest.mark.skip(reason="need to update to otter-grader 6")
+async def test_grade_assignment_8x(configure):
+    secrets_file = os.path.join(os.path.dirname(__file__), KEY_FILE_PATH)
     args = {
         "course": "8x",
         "section": "1",
@@ -30,6 +37,23 @@ async def test_grade_assignment(configure):
         "autograder_materials_repo": "github.com/data-8/materials-x22-private"
     }
     grade, _ = await ga.grade_assignment("tests/test_files/lab01.ipynb",
+                                         args,
+                                         sops_path="sops",
+                                         secrets_file=secrets_file,
+                                         save_path=".")
+    assert 1.0 == grade
+
+
+@pytest.mark.asyncio
+async def test_grade_assignment_88e(configure):
+    secrets_file = os.path.join(os.path.dirname(__file__), KEY_FILE_PATH)
+    args = {
+        "assignment": "lab01",
+        "course": "88ex",
+        "section": "1",
+        "autograder_materials_repo": "github.com/data-88e/Data88e-online-dev"
+    }
+    grade, _ = await ga.grade_assignment("tests/test_files/lab01-88e.ipynb",
                                          args,
                                          sops_path="sops",
                                          secrets_file=secrets_file,
